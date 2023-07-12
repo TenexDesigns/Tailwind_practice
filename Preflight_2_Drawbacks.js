97b6e56bad4fc7803c3c506ea28a6b31312c4a5f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-12 20:48:13 +0300

    Update Preflight_2_Drawbacks.js

--- Preflight_2_Drawbacks.js
+++ Preflight_2_Drawbacks.js
@@ -16,6 +16,23 @@ These are just a few examples of HTML elements that may require additional styli
 Remember that disabling Preflight gives you more control over the styles applied to HTML elements, but it also means that you need to define those styles manually or use Tailwind utility classes for consistent and predictable styling.
 
 
+MORE EXPLANANTION
+  ********************************************************************************************************
+
+  Preflight in Tailwind CSS is a set of base styles designed to smooth over cross-browser inconsistencies and make it easier for you to work within the constraints of your design system docs.w3cub.com. It's built on top of modern-normalize, and Tailwind automatically injects these styles when you include @tailwind base in your CSS docs.w3cub.com.
+
+Preflight removes all the default margins, font sizes, etc., for all your elements like your headings, blockquotes, lists, and so on blog.logrocket.com. This means that everything is unstyled and you start with a blank slate, which is great if you want to design from scratch. However, this might be surprising when you first encounter it docs.w3cub.com.
+
+If you disable Preflight, you are essentially disabling this base style reset. This could be useful if you're integrating Tailwind into an existing project that already has its own base styles, or if you'd like to provide your own base styles tailwindcss.com, docs.w3cub.com, blog.logrocket.com.
+
+You can disable Preflight by setting preflight to false in the corePlugins section of your tailwind.config.js file:
+
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  }
+}
+
 
 
   ...
