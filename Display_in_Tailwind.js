6fdb34ea871a504917319fb19441a5b73f77f6de
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-12 22:32:06 +0300

    Create Display_in_Tailwind.js

---
+++ Display_in_Tailwind.js
@@ -0,0 +1,45 @@
+Sure! Here's an explanation of the display options available in Tailwind CSS:
+
+block: The element takes up the full width available and starts on a new line.
+
+inline-block: The element takes up only as much width as necessary and does not start on a new line. However, it allows other elements to be placed alongside it horizontally.
+
+inline: The element takes up only as much width as necessary and does not start on a new line. It does not allow other elements to be placed alongside it horizontally.
+
+flex: The element becomes a flex container, allowing you to use flexbox properties to control the layout of its child elements.
+
+inline-flex: Similar to flex, but the element behaves as an inline-level flex container.
+
+table: The element behaves like a table.
+
+inline-table: Similar to table, but the element behaves as an inline-level table.
+
+table-caption: The element is displayed as a table caption.
+
+table-cell: The element is displayed as a table cell.
+
+table-column: The element is displayed as a table column.
+
+table-column-group: The element is displayed as a table column group.
+
+table-footer-group: The element is displayed as a table footer group.
+
+table-header-group: The element is displayed as a table header group.
+
+table-row-group: The element is displayed as a table row group.
+
+table-row: The element is displayed as a table row.
+
+flow-root: Creates a new block formatting context for the element. It is useful for clearing floats and containing child elements.
+
+grid: The element becomes a grid container, allowing you to use grid properties to control the layout of its child elements.
+
+inline-grid: Similar to grid, but the element behaves as an inline-level grid container.
+
+contents: The element is removed from the document flow, and its children are treated as if they were direct children of the element's parent.
+
+list-item: The element is displayed as a list item.
+
+hidden: The element is not displayed on the page.
+
+These display options can be applied to different HTML elements using the corresponding utility classes provided by Tailwind CSS. They help you control the layout and positioning of elements in your web page.
