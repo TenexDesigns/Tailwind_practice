6bfefd087dafc7c5c788d1e173caaa2dff415ccc
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-19 17:15:43 +0300

    Create Use_Emojis_or_unicode.js

---
+++ Use_Emojis_or_unicode.js
@@ -0,0 +1,20 @@
+<style>
+    .listTwo{
+        list-style-type: '\1F44D'; // This makes the unordered list have a disk style of theumbs up emoki, Use this uinoce with a forwad faceing slash and without the U+ in the orginal unice U+1F44D)
+    }
+
+<style>
+
+
+<body>
+    Hello 
+
+    <ul class="listTwo">
+        <li>george</li>
+        <li>george</li>
+        <li>george</li>
+        <li>george</li>
+    </ul>
+</body>
+
+
